8b5d805ecdca37ae9a44781af294b04c5c88a8b5
x2zz <fluxtynn@gmail.com>
2026-08-13 22:13:47 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,15 @@ import { createMDX } from 'fumadocs-mdx/next';
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/Obsidian_Documentation',
+
+  images: {
+    unoptimized: true,
+  },
+
   serverExternalPackages: ['typescript', 'twoslash'],
+
   async rewrites() {
     return [
       {
